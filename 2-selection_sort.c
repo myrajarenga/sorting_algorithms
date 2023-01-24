01a5064f5e377ebfefd28c7971a0306de992e099
myrajarenga <myrajarenga1234@gmail.com>
2023-01-24 03:07:07 -0800

    selection-sort implimentation

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx, temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		temp = array[min_idx];
+		array[min_idx] = array[i];
+		array[i] = temp;
+		print_array(array, size); /*printing array after swap*/
+	}
+}

2aa5c411e0da4f9ab43b4a8f94bedb89eeb9bcda
myrajarenga <myrajarenga1234@gmail.com>
2023-01-27 00:13:26 -0800

    selectionsort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,8 +21,7 @@ void swap(int *a, int *b)
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int min_idx, temp;
+	size_t i, j, min_idx;
 
 	if (array == NULL || size < 2)
 		return;
@@ -30,13 +29,13 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
-		for (j = i+ 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
-			if (min_idx != i)
-			swap(&array[mid_idx], &array[i])
-			print_array(array, size);
 		}
+		if (min_idx != i) 
+		swap(&array[min_idx], &array[i]);
+		print_array(array, size);
 	}
 }

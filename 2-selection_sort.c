4acd2063acacf9745bf2fc4b3a32a40cd1d398b6
myrajarenga <myrajarenga1234@gmail.com>
2023-01-26 23:40:14 -0800

    selectionsort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,7 +10,11 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx, temp;
+	size_t i, j;
+	int min_idx, temp;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{

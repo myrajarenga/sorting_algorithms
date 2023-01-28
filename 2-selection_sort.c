8876d7c2ee095bfeba42e9dfeadd471443d31845
myrajarenga <myrajarenga1234@gmail.com>
2023-01-28 00:03:39 -0800

    selction sort implimentstion

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,21 +21,22 @@ void swap(int *a, int *b)
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
+	int *min;
+	size_t i, j;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_idx = i;
+		min = array + i;
 		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
 		{
-			if (array[j] < array[min_idx])
-				min_idx = j;
+			swap(array + i, min);
+			print_array(array, size);
 		}
-		if (min_idx != i) 
-		swap(&array[min_idx], &array[i]);
-		print_array(array, size);
 	}
 }
